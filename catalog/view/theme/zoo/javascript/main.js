004ee3ac716cd8eaba61707b9a0dfc1b20595df5
Orion55 <grol55@mail.ru>
2018-06-27 00:12:44 +0500

    footer

--- catalog/view/theme/zoo/javascript/main.js
+++ catalog/view/theme/zoo/javascript/main.js
@@ -1,45 +1,57 @@
 "use strict";
 
 $(document).ready(function () {
-    if ($(window).width() < 767) {
-        var $menu = $("#my-menu").mmenu({
-            // extensions: ["fullscreen", "theme-dark"]
-            "extensions": ["fx-menu-zoom", "fx-panels-zoom", "pagedim-black", "theme-dark"]
-        });
-        var $icon = $("#mmenu-icon");
-        var API = $menu.data("mmenu");
-        // var logo = $('.logo');
-        // var phone = $('.phone');
-        var title = $('a.mm-navbar__title');
+    var doResize = false;
 
-        title.text('Меню');
+    checkWidth();
 
-        $icon.on("click", function () {
-            API.open();
-        });
+    $(window).resize(function () {
+        checkWidth();
+    });
 
-        $("a.nav__link").click(function () {
-            API.close();
-        });
+    function checkWidth() {
 
-        API.bind("open:finish", function () {
-            setTimeout(function () {
-                $icon.addClass("is-active");
-            }, 100);
+        if ($(window).width() < 767) {
+            if (!doResize) {
+                var $menu = $("#my-menu").mmenu({
+                    "extensions": ["fx-menu-zoom", "fx-panels-zoom", "pagedim-black", "theme-dark"]
+                });
+                var $icon = $("#mmenu-icon");
+                var API = $menu.data("mmenu");
+                var title = $('a.mm-navbar__title');
 
-            $icon.on("click", function () {
-                API.close();
-            });
-        });
+                title.text('Меню');
 
-        API.bind("close:finish", function () {
-            setTimeout(function () {
-                $icon.removeClass("is-active");
-            }, 100);
+                $icon.on("click", function () {
+                    API.open();
+                });
 
-            $icon.on("click", function () {
-                API.open();
-            });
-        });
+                $("a.nav__link").click(function () {
+                    API.close();
+                });
+
+                API.bind("open:finish", function () {
+                    setTimeout(function () {
+                        $icon.addClass("is-active");
+                    }, 100);
+
+                    $icon.on("click", function () {
+                        API.close();
+                    });
+                });
+
+                API.bind("close:finish", function () {
+                    setTimeout(function () {
+                        $icon.removeClass("is-active");
+                    }, 100);
+
+                    $icon.on("click", function () {
+                        API.open();
+                    });
+                });
+
+                doResize = true;
+            }
+        }
     }
 });
